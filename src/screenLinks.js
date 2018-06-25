aff6ea6b76bf07872efa78692879abc36b0dcc14
switchnollie <tim@Tims-MacBook-Pro.local>
2018-06-25 18:28:12 +0200

    Added screen linking configs

---
+++ src/screenLinks.js
@@ -0,0 +1,352 @@
+import Setting0 from "./assets/screens/Settings.png";
+import Setting4 from "./assets/screens/Settings Copy 4.png";
+import Setting5 from "./assets/screens/Settings Copy 5.png";
+import Setting6 from "./assets/screens/Settings Copy 6.png";
+import Setting8 from "./assets/screens/Settings Copy 8.png";
+import Setting9 from "./assets/screens/Settings Copy 9.png";
+import Setting10 from "./assets/screens/Settings Copy 10.png";
+import Setting11 from "./assets/screens/Settings Copy 11.png";
+import Setting12 from "./assets/screens/Settings Copy 12.png";
+import Setting13 from "./assets/screens/Settings Copy 13.png";
+import Setting14 from "./assets/screens/Settings Copy 14.png";
+import Setting15 from "./assets/screens/Settings Copy 15.png";
+import Setting16 from "./assets/screens/Settings Copy 16.png";
+import Setting17 from "./assets/screens/Settings Copy 17.png";
+import Setting18 from "./assets/screens/Settings Copy 18.png";
+import Setting20 from "./assets/screens/Settings Copy 20.png";
+import Setting21 from "./assets/screens/Settings Copy 21.png";
+import Setting22 from "./assets/screens/Settings Copy 22.png";
+import Setting23 from "./assets/screens/Settings Copy 23.png";
+import Setting24 from "./assets/screens/Settings Copy 24.png";
+import Setting25 from "./assets/screens/Settings Copy 25.png";
+import Setting26 from "./assets/screens/Settings Copy 26.png";
+import Setting27 from "./assets/screens/Settings Copy 27.png";
+import Setting28 from "./assets/screens/Settings Copy 28.png";
+import Setting29 from "./assets/screens/Settings Copy 29.png";
+import Setting30 from "./assets/screens/Settings Copy 30.png";
+import Setting31 from "./assets/screens/Settings Copy 31.png";
+import Setting32 from "./assets/screens/Settings Copy 32.png";
+import Setting33 from "./assets/screens/Settings Copy 33.png";
+import Setting34 from "./assets/screens/Settings Copy 34.png";
+
+const screens = {
+  byId: {
+    Setting0: {
+      name: "Setting0",
+      img: Setting0,
+      hotspots: [
+        { x: 0, y: 60, width: 222, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 222, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 222, height: 60, to: "Setting6" }
+      ]
+    },
+    Setting4: {
+      name: "Setting4",
+      img: Setting4,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 472, y: 74, width: 175, height: 98, to: "Setting15" }
+      ]
+    },
+    Setting5: {
+      name: "Setting5",
+      img: Setting5,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 78, y: 74, width: 175, height: 98, to: "Setting12" }
+      ]
+    },
+    Setting6: {
+      name: "Setting6",
+      img: Setting6,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 275, y: 74, width: 175, height: 98, to: "Setting8" }
+      ]
+    },
+    Setting8: {
+      name: "Setting8",
+      img: Setting8,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 57, y: 118, width: 222, height: 60, to: "Setting9" }
+      ]
+    },
+    Setting9: {
+      name: "Setting9",
+      img: Setting9,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 537, y: 80, width: 110, height: 32, to: "Setting10" },
+        { x: 537, y: 132, width: 110, height: 32, to: "Setting11" }
+      ]
+    },
+    Setting10: {
+      name: "Setting10",
+      img: Setting10,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 73, to: "Setting9" },
+        { x: 0, y: 74, width: 184, height: 154, to: "Setting9" },
+        { x: 483, y: 74, width: 184, height: 154, to: "Setting9" }
+      ]
+    },
+    Setting11: {
+      name: "Setting11",
+      img: Setting11,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 73, to: "Setting9" },
+        { x: 0, y: 74, width: 182, height: 231, to: "Setting9" },
+        { x: 485, y: 74, width: 182, height: 231, to: "Setting9" },
+        { x: 0, y: 305, width: 667, height: 70, to: "Setting9" },
+        { x: 204, y: 251, width: 110, height: 32, to: "Setting9" },
+        { x: 354, y: 251, width: 110, height: 32, to: "Setting8" }
+      ]
+    },
+    Setting12: {
+      name: "Setting12",
+      img: Setting12,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 98, y: 263, width: 150, height: 32, to: "Setting14" },
+        { x: 401, y: 263, width: 150, height: 32, to: "Setting13" }
+      ]
+    },
+    Setting13: {
+      name: "Setting13",
+      img: Setting13,
+      hotspots: [{ x: 0, y: 0, width: 667, height: 375, to: "Setting12" }]
+    },
+    Setting14: {
+      name: "Setting14",
+      img: Setting14,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 73, to: "Setting12" },
+        { x: 0, y: 74, width: 184, height: 154, to: "Setting12" },
+        { x: 483, y: 74, width: 184, height: 154, to: "Setting12" }
+      ]
+    },
+    Setting15: {
+      name: "Setting15",
+      img: Setting15,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 57, y: 118, width: 222, height: 60, to: "Setting23" },
+        { x: 57, y: 177, width: 222, height: 60, to: "Setting21" },
+        { x: 57, y: 236, width: 222, height: 60, to: "Setting22" },
+        { x: 537, y: 80, width: 110, height: 32, to: "Setting16" },
+        { x: 537, y: 132, width: 110, height: 32, to: "Setting20" }
+      ]
+    },
+    Setting16: {
+      name: "Setting16",
+      img: Setting16,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 71, to: "Setting15" },
+        { x: 0, y: 76, width: 181, height: 231, to: "Setting15" },
+        { x: 486, y: 76, width: 181, height: 231, to: "Setting15" },
+        { x: 0, y: 304, width: 667, height: 71, to: "Setting15" },
+        { x: 204, y: 95, width: 261, height: 46, to: "Setting17" },
+        { x: 204, y: 161, width: 261, height: 47, to: "Setting18" }
+      ]
+    },
+    Setting17: {
+      name: "Setting17",
+      img: Setting17,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 73, to: "Setting15" },
+        { x: 0, y: 74, width: 184, height: 154, to: "Setting15" },
+        { x: 483, y: 74, width: 184, height: 154, to: "Setting15" },
+        { x: 204, y: 161, width: 261, height: 47, to: "Setting18" }
+      ]
+    },
+    Setting18: {
+      name: "Setting18",
+      img: Setting18,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 71, to: "Setting15" },
+        { x: 0, y: 76, width: 181, height: 231, to: "Setting15" },
+        { x: 486, y: 76, width: 181, height: 231, to: "Setting15" },
+        { x: 195, y: 132, width: 129, height: 134, to: "Setting24" },
+        { x: 341, y: 132, width: 129, height: 134, to: "Setting25" }
+      ]
+    },
+    Setting20: {
+      name: "Setting20",
+      img: Setting20,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 71, to: "Setting15" },
+        { x: 0, y: 76, width: 181, height: 231, to: "Setting15" },
+        { x: 486, y: 76, width: 181, height: 231, to: "Setting15" },
+        { x: 204, y: 251, width: 110, height: 32, to: "Setting15" },
+        { x: 354, y: 251, width: 110, height: 32, to: "Setting4" }
+      ]
+    },
+    Setting21: {
+      name: "Setting21",
+      img: Setting21,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 57, y: 118, width: 222, height: 60, to: "Setting23" },
+        { x: 57, y: 177, width: 222, height: 60, to: "Setting21" },
+        { x: 57, y: 236, width: 222, height: 60, to: "Setting22" },
+        { x: 298, y: 80, width: 165, height: 92, to: "Setting30" }
+      ]
+    },
+    Setting22: {
+      name: "Setting22",
+      img: Setting22,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 57, y: 118, width: 222, height: 60, to: "Setting23" },
+        { x: 57, y: 177, width: 222, height: 60, to: "Setting21" },
+        { x: 57, y: 236, width: 222, height: 60, to: "Setting22" },
+        { x: 341, y: 221, width: 110, height: 32, to: "Setting5" }
+      ]
+    },
+    Setting23: {
+      name: "Setting23",
+      img: Setting23,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 57, y: 118, width: 222, height: 60, to: "Setting23" },
+        { x: 57, y: 177, width: 222, height: 60, to: "Setting21" },
+        { x: 57, y: 236, width: 222, height: 60, to: "Setting22" },
+        { x: 537, y: 80, width: 110, height: 32, to: "Setting26" }
+      ]
+    },
+    Setting24: {
+      name: "Setting24",
+      img: Setting24,
+      hotspots: [{ x: 0, y: 0, width: 667, height: 375, to: "Setting18" }]
+    },
+    Setting25: {
+      name: "Setting25",
+      img: Setting25,
+      hotspots: [{ x: 0, y: 0, width: 667, height: 375, to: "Setting18" }]
+    },
+    Setting26: {
+      name: "Setting26",
+      img: Setting26,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 71, to: "Setting23" },
+        { x: 0, y: 76, width: 181, height: 231, to: "Setting23" },
+        { x: 486, y: 76, width: 181, height: 231, to: "Setting23" },
+        { x: 204, y: 251, width: 110, height: 32, to: "Setting23" }
+      ]
+    },
+    Setting27: {
+      name: "Setting27",
+      img: Setting27,
+      hotspots: [{ x: 582, y: 22, width: 74, height: 37, to: "Setting23" }]
+    },
+    Setting28: {
+      name: "Setting28",
+      img: Setting28,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 28, to: "Setting23" },
+        { x: 0, y: 28, width: 181, height: 320, to: "Setting23" },
+        { x: 486, y: 27, width: 181, height: 321, to: "Setting23" },
+        { x: 0, y: 347, width: 667, height: 28, to: "Setting23" },
+        { x: 349, y: 295, width: 110, height: 32, to: "Setting23" }
+      ]
+    },
+    Setting29: {
+      name: "Setting29",
+      img: Setting29,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 28, to: "Setting23" },
+        { x: 0, y: 28, width: 181, height: 320, to: "Setting23" },
+        { x: 486, y: 27, width: 181, height: 321, to: "Setting23" },
+        { x: 0, y: 347, width: 667, height: 28, to: "Setting23" },
+        { x: 279, y: 295, width: 110, height: 32, to: "Setting23" }
+      ]
+    },
+    Setting30: {
+      name: "Setting30",
+      img: Setting30,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 57, y: 118, width: 222, height: 60, to: "Setting23" },
+        { x: 57, y: 177, width: 222, height: 60, to: "Setting21" },
+        { x: 57, y: 236, width: 222, height: 60, to: "Setting22" },
+        { x: 278, y: 118, width: 222, height: 60, to: "Setting31" }
+      ]
+    },
+    Setting31: {
+      name: "Setting31",
+      img: Setting31,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 57, y: 118, width: 222, height: 60, to: "Setting23" },
+        { x: 57, y: 177, width: 222, height: 60, to: "Setting21" },
+        { x: 57, y: 236, width: 222, height: 60, to: "Setting22" },
+        { x: 521, y: 118, width: 50, height: 24, to: "Setting32" }
+      ]
+    },
+    Setting32: {
+      name: "Setting32",
+      img: Setting32,
+      hotspots: [
+        { x: 0, y: 60, width: 58, height: 60, to: "Setting4" },
+        { x: 0, y: 119, width: 58, height: 60, to: "Setting5" },
+        { x: 0, y: 178, width: 58, height: 60, to: "Setting6" },
+        { x: 57, y: 118, width: 222, height: 60, to: "Setting23" },
+        { x: 57, y: 177, width: 222, height: 60, to: "Setting21" },
+        { x: 57, y: 236, width: 222, height: 60, to: "Setting22" },
+        { x: 521, y: 118, width: 50, height: 24, to: "Setting31" },
+        { x: 521, y: 228, width: 110, height: 32, to: "Setting33" }
+      ]
+    },
+    Setting33: {
+      name: "Setting33",
+      img: Setting33,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 28, to: "Setting32" },
+        { x: 0, y: 28, width: 181, height: 320, to: "Setting32" },
+        { x: 486, y: 27, width: 181, height: 321, to: "Setting32" },
+        { x: 0, y: 347, width: 667, height: 28, to: "Setting32" },
+        { x: 279, y: 293, width: 110, height: 32, to: "Setting32" },
+        { x: 309, y: 75, width: 50, height: 24, to: "Setting34" }
+      ]
+    },
+    Setting34: {
+      name: "Setting34",
+      img: Setting34,
+      hotspots: [
+        { x: 0, y: 0, width: 667, height: 28, to: "Setting32" },
+        { x: 0, y: 28, width: 181, height: 320, to: "Setting32" },
+        { x: 486, y: 27, width: 181, height: 321, to: "Setting32" },
+        { x: 0, y: 347, width: 667, height: 28, to: "Setting32" },
+        { x: 279, y: 293, width: 110, height: 32, to: "Setting32" },
+        { x: 309, y: 75, width: 50, height: 24, to: "Setting33" }
+      ]
+    }
+  },
+  allIds: ["Setting0", "Setting4"]
+};
+
+export default screens;
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
+import screens from "../screenLinks";
 
 export default class Settings extends Component {
+  state = {
+    currentScreen: "Setting0"
+  };
   render() {
-    return <div>Hello from Settings</div>;
+    const currentScreenObj = screens.byId[this.state.currentScreen];
+    console.log(this.state.currentScreen);
+    console.log(screens);
+    console.log(screens.byId[this.state.currentScreen]);
+    return (
+      <div>
+        <img
+          style={{ position: "relative", width: "667px", height: "375px" }}
+          src={currentScreenObj.img}
+          className="mockupScreen"
+        />
+        {currentScreenObj.hotspots.map((hotspot, index) => (
+          <div
+            key={index}
+            style={{
+              background: "rgba(255,0,0,0.1)",
+              position: "absolute",
+              left: hotspot.x,
+              top: hotspot.y,
+              width: hotspot.width,
+              height: hotspot.height
+            }}
+            onClick={() => this.setState({ currentScreen: hotspot.to })}
+          />
+        ))}
+      </div>
+    );
   }
 }
